e3aa1f0c3e7a5c5d79ec6a067ae2cf82be79738c
Benson Rajan <brajan@UNFCU.com>
2016-07-22 13:10:40 -0400

    1st Commit

---
+++ SimpleApp/SimpleApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleApp.Startup))]
+namespace SimpleApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
